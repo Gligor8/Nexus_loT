7a005d599ba99f7262cc97a6f6b91d3d78b441a7
kovacevski <d.kovacevski@yahoo.com>
2022-08-30 16:00:31 +0200

    feat: TestAPI

---
+++ TestApi/Controllers/SensorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TestApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SensorController : ControllerBase
+    {
+        [HttpGet("GetPlainValue")]
+        public double GetPlainValue()
+        {
+            return GetRandomValue();
+        }
+
+        [HttpGet("GetJsonValue")]
+        public JsonResult GetJsonValue()
+        {
+            return new JsonResult(new { temperature = GetRandomValue() });
+        }
+
+        [HttpGet("GetJsonComplexValue")]
+        public JsonResult GetJsonComplexValue()
+        {
+            return new JsonResult(new { temperature = GetRandomValue(), humidity = GetRandomValue(), });
+        }
+
+        private static double GetRandomValue()
+        {
+            var randomNumber = new Random().NextDouble() * 50;
+            return Math.Round(randomNumber, 1);
+        }
+    }
+}
